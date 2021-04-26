2ceb51c914ad157a8643aa64777ef54bf7d4ab71
feysalaf <faisalyusuf98@gmail.com>
2021-04-26 22:40:00 +0500

    Added Registration

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './registration/registration.component';
+import { FeedComponent } from './feed/feed.component';
+import { FollowingComponent } from './following/following.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationComponent,
+    FeedComponent,
+    FollowingComponent
   ],
   imports: [
     BrowserModule,
